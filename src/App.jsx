6b290a16abdc48c26261e65f0d4c21c83820c3c0
Rolf M <rolfie@outlook.dk>
2025-02-23 11:53:09 +0100

    Opdateret projekt efter pause

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import AppContext from "./utils/context";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<div>
+			<BrowserRouter>
+				<AppContext>
+					<Routes>
+						<Route path="/home" element={<Home />} />
+					</Routes>
+				</AppContext>
+			</BrowserRouter>
+
+			<Header />
+
+			<h1>hello sexy mama</h1>
+		</div>
+	);
+}
+
+export default App;
